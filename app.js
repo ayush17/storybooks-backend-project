c0d8d3b35213575bc252bc2196adc63b7d4f8288
Ayush Tiwari <ayushtiwari@Ayushs-MacBook-Pro.local>
2023-02-12 15:14:52 +0530

    add login and logout with Oauth and data of user stored in mongo

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 //Loads the handlebars module
 const handlebars = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
 const connectDB = require("./config/db");
 //Load config
 
 dotenv.config({ path: "./config/config.env" });
+
+//passport config
+require("./config/passport")(passport);
 
 connectDB();
 
@@ -29,11 +34,24 @@ app.engine(
     extname: ".hbs",
   })
 );
+//Sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
 app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 3000;
 
